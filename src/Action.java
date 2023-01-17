300c0bd3f04bb80b8264da3e6d652aa1dda154ad
Anna Skripkina <skripwomen@gmail.com>
2023-01-17 11:07:34 +0100

    тут пока все работает

--- src/Action.java
+++ src/Action.java
@@ -7,34 +7,28 @@ import java.util.*;
 
 public class Action {
 
-    public static List<String> encode(int nummerOffset) throws IOException {
+    public static List<String> encode(int numberOffset) throws IOException {
+        int charNumber = 0;
+        List<Character> listCollectingCharacter = new ArrayList<>();
+        List<String> encodedlist = null;
 
         Path sourceText = Path.of("C:\\\\Users\\PC\\IdeaProjects\\CoderProject\\src\\Block.txt");
         Path encodedText = Path.of("C:\\\\Users\\PC\\IdeaProjects\\CoderProject\\src\\Block2.txt");
         List<String> sourceList = Files.readAllLines(sourceText);
-        List<Character> listCollectingCharacter = new ArrayList<>();
-        System.out.println("Это наш исходный текст:");
-        List<String> encodedlist = null;
-        for (String str : sourceList) {                                           // здесь я вывожу на экран исходный текст
-            System.out.println(str);
-        }
+
         for (String str : sourceList) {                                           // здесь я перебираю строки моего незашифрованного текста
             char[] chars = str.toCharArray();                                     // каждую строку представляю массивом символов
-            for (int i = 0; i < chars.length; i++) {                              //перебираю массив символов
+            for (char aChar : chars) {                                            //перебираю массив символов
                 for (int j = 1; j < Constants.characterPattern().size(); j++) {   // перебираю список образца алфавата
-                    int k = 0;
-                    if (chars[i] == Constants.characterPattern().get(j)) {        // если символ из массива равен символу из списка
-                        k = j + nummerOffset;                                     // создаю индекс смещения для шифра
-                        if (k > Constants.characterPattern().size()) {            // если индекс выйдет за границы моего образца алфавита
-                            k = k - 1 - Constants.characterPattern().size();      // то рассчитаю на сколько вышло и это будет новый коэффициент смещения
-                        }
-                        listCollectingCharacter.add(Constants.characterPattern().get(k));
 
+                    if (aChar == Constants.characterPattern().get(j)) {           // если символ из массива равен символу из списка
+                        charNumber = j;
+                        listCollectingCharacter.add(Constants.characterPattern().get(Action.encodingFactor(numberOffset, charNumber)));
                         StringBuilder sb = new StringBuilder();
                         for (Character ch : listCollectingCharacter) {
                             sb.append(ch);
                         }
-                        Files.writeString(encodedText, sb + "\r");
+                        Files.writeString(encodedText, sb);
                     }
                 }
             }
@@ -43,34 +37,46 @@ public class Action {
         return encodedlist;
     }
 
-    public static List<String> decode(int nummerOffset) throws IOException {
+    public static int encodingFactor(int numberOffset, int charNumber) {
+        int encodingFactor = charNumber + numberOffset;                                     // создаю индекс смещения для шифра
+        if (encodingFactor > Constants.characterPattern().size()) {                         // если индекс выйдет за границы моего образца алфавита
+            encodingFactor = encodingFactor - 1 - Constants.characterPattern().size();      // то рассчитаю на сколько вышло и это будет новый коэффициент смещения
+        }
+        return encodingFactor;
+    }
+
+    public static StringBuilder decode(int nummerOffset) throws IOException {
         List<Character> listCollectingCharacter = new ArrayList<>();
         List<String> decodedlist = null;
+        int charNumber = 0;
+        StringBuilder sb = null;
         Path encodedText = Path.of("C:\\\\Users\\PC\\IdeaProjects\\CoderProject\\src\\Block2.txt");
         List<String> encodedList = Files.readAllLines(encodedText);
 
-        for (String str : encodedList) {                                           // здесь я перебираю строки моего незашифрованного текста
+        for (String str : encodedList) {                                          // здесь я перебираю строки моего незашифрованного текста
             char[] chars = str.toCharArray();                                     // каждую строку представляю массивом символов
-            for (int i = 0; i < chars.length; i++) {                              //перебираю массив символов
+            for (char aChar : chars) {                                            //перебираю массив символов
                 for (int j = 1; j < Constants.characterPattern().size(); j++) {   // перебираю список образца алфавата
-                    int k = 0;
-                    if (chars[i] == Constants.characterPattern().get(j)) {        // если символ из массива равен символу из списка
-                        k = j - nummerOffset;                                     // создаю индекс смещения для шифра
-                        if (k < 0) {            // если индекс выйдет за границы моего образца алфавита
-                            k = Constants.characterPattern().size() + k;      // то рассчитаю на сколько вышло и это будет новый коэффициент смещения
-                        }
-                        listCollectingCharacter.add(Constants.characterPattern().get(k));
-
-                        StringBuilder sb = new StringBuilder();
+                    int encodingFactor = 0;
+                    if (aChar == Constants.characterPattern().get(j)) {           // если символ из массива равен символу из списка
+                        charNumber = j;
+                        listCollectingCharacter.add(Constants.characterPattern().get(Action.decodingFactor(nummerOffset, charNumber)));
+                         sb = new StringBuilder();
                         for (Character ch : listCollectingCharacter) {
                             sb.append(ch);
                         }
-                        Files.writeString(encodedText, sb + "\r");
                     }
                 }
             }
         }
-        decodedlist = Files.readAllLines(encodedText);
-        return decodedlist;
+        return sb;
+    }
+
+    public static int decodingFactor(int numberOffset, int charNumber) {
+        int decodingFactor =   charNumber-numberOffset;                                 // создаю индекс смещения для шифра
+        if (decodingFactor < 0) {                                                       // если индекс выйдет за границы моего образца алфавита
+            decodingFactor = Constants.characterPattern().size() + decodingFactor;      // то рассчитаю на сколько вышло и это будет новый коэффициент смещения
+        }
+        return decodingFactor;
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -18,12 +18,25 @@ public class Solution {
         if (!console.hasNextInt()) {
             System.out.println("Нет, нет, введи именно цифру! Это важно \uD83D\uDE00");
         }
-        int nummerOffset = console.nextInt();
+        int numberOffset = console.nextInt();
 
-        for (String st : Action.encode(nummerOffset)) {
+        Path sourceText = Path.of("C:\\\\Users\\PC\\IdeaProjects\\CoderProject\\src\\Block.txt");
+        System.out.println("Это наш исходный текст:");
+
+        List<String> sourceList = Files.readAllLines(sourceText);
+        for (String str : sourceList) {
+            System.out.println(str);
+        }
+
+        System.out.println("\n Закодированный текст:\n ");
+        for (String st : Action.encode(numberOffset)) {
             System.out.println(st);
         }
-        System.out.println(Action.decode(nummerOffset).toString());
+
+        System.out.println("\n Раскодируем обратно:\n ");
+        System.out.println(Action.decode(numberOffset));
+
+
 
 
     }
